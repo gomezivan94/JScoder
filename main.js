01a2b88507137599bfc7584407718043408236c7
Ivan Gomez <gi.gomezivan@gmail.com>
2023-02-01 22:19:10 -0300

    js actualizado

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function genero(){
 } 
 
 function saludar(){
-    if(genero === "M"){
+    if(genero === "M" || "m"){
         alert("Bienvenido " + cliente)
     } else{
         alert("Bienvenida " + cliente)
@@ -24,10 +24,39 @@ function seleccionarProducto(){
     } else {
         alert("Usted selecciono Post training")
     }
+    opcion = prompt("Vuelva a ingresar una opcion \n 2: Mostrar total \n 3: Salir")
 }
 
+let creatina = 500
+let proteina = 800
+let post = 400
+
+function finalizarCompra(){
+    if (producto === "1"){
+        alert("Creatina 300mg en el carrito por un total de " + creatina * 1.21)
+    }
+    else if(producto === "2"){
+        alert("Proteina 1kg en el carrito por un total de " + proteina * 1.21)
+    }
+    else if(producto === "3"){
+        alert("Post Training en el carrito por un total de " + post * 1.21)
+    }
+}
 
 cliente()
 genero()
 saludar()
-seleccionarProducto()
+
+
+let opcion = prompt("1: Comprar suplemento \n 2: Finalizar Compra \n 3: Salir")
+
+while(opcion !== "3"){
+    if(opcion === "1"){
+        seleccionarProducto()
+    } else if(opcion === "2"){
+        finalizarCompra()
+        opcion = "3"
+    }
+}
+
+alert("Gracias por su compra, vuelvas prontos")
