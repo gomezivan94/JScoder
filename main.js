97ba7853df195eef38950168710362006e1b108e
Ivan Gomez <gi.gomezivan@gmail.com>
2023-02-05 21:24:46 -0300

    agregue sass y cambios en hero

--- main.js
+++ main.js
@@ -1,62 +1,73 @@
-function cliente(){
-    cliente = prompt("Ingrese su nombre");
+// function cliente(){
+//     cliente = prompt("Ingrese su nombre");
+// }
+
+// function genero(){
+//     genero = prompt("Cual es su genero? \n M para masculino \n F para femenino");
+// } 
+
+// function saludar(){
+//     if(genero === "M" || "m"){
+//         alert("Bienvenido " + cliente)
+//     } else{
+//         alert("Bienvenida " + cliente)
+//     }
+// }
+
+// function seleccionarProducto(){
+//     producto = prompt("Elija su producto \n 1: Creatina 300mg \n 2: Proteina 1kg \n 3: Post Training");
+
+//     if (producto === "1"){
+//         alert("Usted selecciono Creatina 300mg")
+//     } else if (producto === "2") {
+//         alert("Usted selecciono Proteina 1kg")
+//     } else {
+//         alert("Usted selecciono Post training")
+//     }
+//     opcion = prompt("Vuelva a ingresar una opcion \n 2: Mostrar total \n 3: Salir")
+// }
+
+// let creatina = 500
+// let proteina = 800
+// let post = 400
+
+// function finalizarCompra(){
+//     if (producto === "1"){
+//         alert("Creatina 300mg en el carrito por un total de " + creatina * 1.21)
+//     }
+//     else if(producto === "2"){
+//         alert("Proteina 1kg en el carrito por un total de " + proteina * 1.21)
+//     }
+//     else if(producto === "3"){
+//         alert("Post Training en el carrito por un total de " + post * 1.21)
+//     }
+// }
+
+// cliente()
+// genero()
+// saludar()
+
+
+// let opcion = prompt("1: Comprar suplemento \n 2: Finalizar Compra \n 3: Salir")
+
+// while(opcion !== "3"){
+//     if(opcion === "1"){
+//         seleccionarProducto()
+//     } else if(opcion === "2"){
+//         finalizarCompra()
+//         opcion = "3"
+//     }
+// }
+
+// alert("Gracias por su compra, vuelvas prontos")
+
+function texto() {var typed2 = new Typed('.typed2', {
+    strings: ["Work Hard", "Play Hard"],
+    typeSpeed: 100,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: true
+  })
 }
 
-function genero(){
-    genero = prompt("Cual es su genero? \n M para masculino \n F para femenino");
-} 
-
-function saludar(){
-    if(genero === "M" || "m"){
-        alert("Bienvenido " + cliente)
-    } else{
-        alert("Bienvenida " + cliente)
-    }
-}
-
-function seleccionarProducto(){
-    producto = prompt("Elija su producto \n 1: Creatina 300mg \n 2: Proteina 1kg \n 3: Post Training");
-
-    if (producto === "1"){
-        alert("Usted selecciono Creatina 300mg")
-    } else if (producto === "2") {
-        alert("Usted selecciono Proteina 1kg")
-    } else {
-        alert("Usted selecciono Post training")
-    }
-    opcion = prompt("Vuelva a ingresar una opcion \n 2: Mostrar total \n 3: Salir")
-}
-
-let creatina = 500
-let proteina = 800
-let post = 400
-
-function finalizarCompra(){
-    if (producto === "1"){
-        alert("Creatina 300mg en el carrito por un total de " + creatina * 1.21)
-    }
-    else if(producto === "2"){
-        alert("Proteina 1kg en el carrito por un total de " + proteina * 1.21)
-    }
-    else if(producto === "3"){
-        alert("Post Training en el carrito por un total de " + post * 1.21)
-    }
-}
-
-cliente()
-genero()
-saludar()
-
-
-let opcion = prompt("1: Comprar suplemento \n 2: Finalizar Compra \n 3: Salir")
-
-while(opcion !== "3"){
-    if(opcion === "1"){
-        seleccionarProducto()
-    } else if(opcion === "2"){
-        finalizarCompra()
-        opcion = "3"
-    }
-}
-
-alert("Gracias por su compra, vuelvas prontos")
+texto()
